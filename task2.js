089eaf92a9f42e18334d4879b3fc6b31e3888a99
Qwerty212112 <87649037+Qwerty212112@users.noreply.github.com>
2025-05-13 14:42:41 +0800

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,30 +1,26 @@
-// Чувство, что моя программа очень большая и какая-то сомвесть неправильная. С учетом того чтом мы еще не проходили цикл while, думаю тут должно быть какое то другое решение
-// Мне в общем не нравится, кажется что есть какой-то Math. который может считать все четные и нечетные или другая команда, но я решил делать из того что знаю
-
-
-function getSumOfNumber (number,type) {
+function getSumOfNumber (number,type = 'odd') {
     let sum = 0;
     let i = 0;
-    let y = 1;
-    
-    if (number == '') {
+
+    console.log(typeof number)
+
+    if (typeof number !== 'number') {
       sum = NaN;
       return sum;
     }
     while (i <= number) {
-      if (type == '') {
+      if (type === '') {
         sum+=i
-        i++
-      } else if (type == 'even') {
+        
+      } else if (type === 'even' && i % 2 === 0) {
         sum+=i
-        i+=2
-      } else if (type == 'odd' || type == undefined && y <= number) {
-        sum+=y
-        y+=2
-        i=y
+        
+      } else if (type === 'odd' && i % 2 === 1) {
+        sum+=i
       }
+      i++
 }
 return sum;
 }
 
-console.log(getSumOfNumber(10))
+console.log(getSumOfNumber(10, ''))
